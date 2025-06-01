7c1ef80d8bb71c1746dc47af3f8d79ebf5283241
zimuya4153 <limulu10@163.com>
2025-06-01 18:32:54 +0800

    feat: support Linux

--- xmake.lua
+++ xmake.lua
@@ -1,34 +1,41 @@
 add_rules("mode.debug", "mode.release")
 
-if not has_config("vs_runtime") then
+if is_plat("windows") and not has_config("vs_runtime") then
     set_runtimes("MD")
 end
 
 target("BinaryStream")
     set_kind("static")
     set_languages("cxx20")
-    set_symbols("debug")   
+    set_symbols("debug")
     set_exceptions("none")
     add_includedirs("include")
-    add_defines(
-        "NOMINMAX", 
-        "UNICODE"
-    )
-    add_cxflags(
-        "/EHa", 
-        "/utf-8", 
-        "/W4", 
-        "/w44265", 
-        "/w44289", 
-        "/w44296", 
-        "/w45263", 
-        "/w44738", 
-        "/w45204", 
-        "/O2", 
-        "/Ob3"
-    )
-    add_ldflags(
-        "/OPT:REF", 
-        "/OPT:ICF"
-    )
     add_files("src/**.cpp")
+    
+    if is_plat("windows") then
+        add_defines("NOMINMAX", "UNICODE")
+        add_cxflags(
+            "/EHa",
+            "/utf-8",
+            "/W4",
+            "/w44265",
+            "/w44289",
+            "/w44296",
+            "/w45263",
+            "/w44738",
+            "/w45204",
+            "/O2",
+            "/Ob3"
+        )
+        add_ldflags(
+            "/OPT:REF",
+            "/OPT:ICF"
+        )
+    else
+        add_cxflags(
+            "-Wall",
+            "-pedantic",
+            "-O2",
+            "-fPIC"
+        )
+    end
